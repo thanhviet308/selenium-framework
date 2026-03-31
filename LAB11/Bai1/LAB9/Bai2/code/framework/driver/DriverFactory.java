045d852996776da6692bba5bfac97f219fd11e9b
Thanh Viet <thanhviet082004@gmail.com>
2026-03-31 14:35:52 +0700

    fix

---
+++ LAB11/Bai1/LAB9/Bai2/code/framework/driver/DriverFactory.java
@@ -0,0 +1,28 @@
+package framework.driver;
+
+import io.github.bonigarcia.wdm.WebDriverManager;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.chrome.ChromeOptions;
+
+public class DriverFactory {
+
+    public static WebDriver createDriver(String browser) {
+        boolean isCI = System.getenv("CI") != null;
+
+        WebDriverManager.chromedriver().setup();
+
+        ChromeOptions options = new ChromeOptions();
+
+        if (isCI) {
+            options.addArguments("--headless=new");
+            options.addArguments("--no-sandbox");
+            options.addArguments("--disable-dev-shm-usage");
+            options.addArguments("--window-size=1920,1080");
+        } else {
+            options.addArguments("--start-maximized");
+        }
+
+        return new ChromeDriver(options);
+    }
+}
